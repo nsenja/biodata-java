777aa24ba7a9d9a31d03f5d78b57f42410e698de
nsenja <nabilasw34@gamil..com>
2021-11-23 14:13:50 +0700

    biodata

---
+++ Latihan.java
@@ -0,0 +1,14 @@
+public class Latihan { 
+    public static void main(String[] args) {
+    System.out.println("----------------------");
+    System.out.println("Biodata Mahasiswa Baru");
+    System.out.println("----------------------");
+    System.out.println("NIM     :");
+    System.out.println("NAMA    : Nabila Senja");
+    System.out.println("ALAMAT  : Malang");
+    System.out.println("HP      : 089692236737");
+    System.out.println("----------------------");
+
+ 
+    }
+}
